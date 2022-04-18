1cd2f5da9233493a95cf7230ead3f110a1bf20f5
zerontec <zerontecnologia1@gmail.com>
2022-04-18 19:29:25 -0430

    List pokemon search button pagination

--- src/pages/Dashboard/Dashborad.jsx
+++ src/pages/Dashboard/Dashborad.jsx
@@ -1,140 +1,161 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import NavbarP from '../../components/Navbar/Navbar';
-import Paginations from '../../components/Paginations/Pagination';
-import { getPokemon, getPokemonList } from '../../Redux/actions';
+import ReactPaginate from 'react-paginate'
+import { getPokemonList } from '../../Redux/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import Card from '../../components/Card/Card';
 import Loading from '../../components/Loading/Loading';
 import _ from "lodash";
 import './ListStyles.css'
+import { Link } from 'react-router-dom';
 
 
 
 
 const Dashboard = (props) => {
- const [search, setSeacrh] = useState();
- const dispatch = useDispatch();
- const PokemonList = useSelector((state) => state.PokemonList);
- 
+
+  const [search, setSearch] = useState();
+  const dispatch = useDispatch();
+  const PokemonList = useSelector((state) => state.PokemonList);
+
 
   //useEfect
 
   useEffect(() => {
     FetchData(1);
-  
+
   }, []);
 
   const FetchData = (page = 1) => {
 
-      dispatch(getPokemonList(page));
+    dispatch(getPokemonList(page));
 
 
   }
 
   const ShowData = () => {
-    if(PokemonList.Loading){
+    if (PokemonList.Loading) {
 
       <p>Loading...</p>
     }
 
-if(!_.isEmpty(PokemonList.data)){
+    if (!_.isEmpty(PokemonList.data)) {
 
 
-return(
-  <div className="card">
-   
-{PokemonList.data.map((el) => {
+      return (
+        <div className="row">
 
 
-return(
-  <div className="card-image">
-    <figure className="Imagea">
-   <img alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${el.name}.jpg`}/>
-    </figure>
+
+          {PokemonList.data.map((el) => {
 
 
-  <div className="card-content">
-  <div className="media">
-      <div className="media-left">
-          <figure className="image is-48x48">
-              <img src={`https://img.pokemondb.net/artwork/large/${el.name}.jpg`}/>
-          </figure>
-      </div>
-      <div className="media-content">
-          <p className="title is-4">{el.name}</p>
-          <p className="subtitle is-6">@1johnsmith</p>
-      </div>
-  </div>
-
-  <div className="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-      <a href="#">#css</a> <a href="#">#responsive</a>
-      <br />
-      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-  </div>
-</div>
-</div>
-)
+            return (
+              <div className="column">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="Imagea">
+                      <img alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${el.name}.jpg`} />
+                    </figure>
 
 
-})}
- 
-</div>
-  
-)
+                    <div className="card-content">
+                      <div className="media">
+                        <div className="media-left">
+                          <figure className="image is-48x48">
+                            <img src={`https://img.pokemondb.net/artwork/large/${el.name}.jpg`} />
+                          </figure>
+                        </div>
+                        <div className="media-content">
+                          <p className="title is-4">{el.name}</p>
+                          <p className="subtitle is-6"><Link to={`/detail/${el.name}`}> Ver</Link></p>
+                        </div>
+                      </div>
 
-}
+                      <div className="content">
+                        Conoce más habilidades y caracteristicas. de <strong>{el.name} </strong> 
+                        <br />
+                        <p className="subtitle is-6"><Link to={`/detail/${el.name}`}> Conoce más</Link></p>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+
+
+          })}
+
+
+        </div>
+
+      )
+
+    }
   }
 
 
 
 
- 
+
   return (
 
 
     <div>
-
+    
       <section className="section">
         <div className='container '>
-
+ 
           <div className="column ">
 
-            <div className="columns  ">
-              <div className="column ">
+{/* search */}
+          <div className='container '>
+         <div className="columns">
+           <div className="column is-four-fifths">
+         <div className="field is-grouped">
+  <p className="control is-expanded">
+    <input className="input" type="text" onChange={(e) => setSearch(e.target.value)} placeholder="Encuentra tu pokemon"/>
+  </p>
+  <p className="control">
+    <button className="button is-primary" onClick={() => props.history.push(`/detail/${search}`)}>
+      Search
+    </button>
+  </p>
+  </div>
+</div>
+</div>
+</div>{/* End search */}
+<hr />
 
+          {ShowData()}
+    
+          </div>
+          {!_.isEmpty(PokemonList.data) && (
 
-<ShowData/>
-
-
-
-              </div>
-
-              
-
-            </div>
-
-
-
-
-
-{/* 
-            <Paginations
-              pokemonPerpage={pokemonPerpage}
-              allPokemons={allPokemons}
-              paginate={paginate}
-
-            /> */}
-
-
+          <div className="container">
+       <hr/>
+                  <ReactPaginate 
+                  
+                  pageCount={Math.ceil(PokemonList.count / 9)}
+                  pageRangeDisplayed={2}
+                  marginPagesDisplayed={1}
+                  onPageChange={(data) => FetchData(data.selected + 1)}
+                  containerClassName={"pagination is-rounded"}
+                 pageClassName={"container"}
+                  previousClassName={"pagination-previous"}
+                  nextClassName={"pagination-next"}
+                  /* activeLinkClassName={"pagination-link"} */
+                  pageLinkClassName={"pagination-link" }
+                  activeLinkClassName={"pagination-link is-current"}
+                  />
+                   
+                   
 
           </div>
+    )}
 
 
         </div>
-
-
       </section>
     </div>
 

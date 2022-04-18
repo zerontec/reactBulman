6951f3bc170fd42af425b6f38819cb7bf233064b
zerontec <zerontecnologia1@gmail.com>
2022-04-18 11:32:02 -0430

    component paginations

--- src/components/Paginations/Pagination.jsx
+++ src/components/Paginations/Pagination.jsx
@@ -3,9 +3,17 @@ import React from "react";
 
 
 
-const Paginations = () => {
+const Paginations = ({pokemonPerpage, allPokemons, paginate}) => {
 
-return(
+
+const pagesNumber = [];
+
+for (let i = 1; i <= Math.ceil(allPokemons / pokemonPerpage); i ++){
+pagesNumber.push(i)
+
+}
+
+  return(
 
 <div>
 
@@ -15,9 +23,9 @@ return(
   <ul className="pagination-list">
     <li><a className="pagination-link" aria-label="Goto page 1">1</a></li>
     <li><span className="pagination-ellipsis">&hellip;</span></li>
-    <li><a className="pagination-link" aria-label="Goto page 45">45</a></li>
-    <li><a className="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
-    <li><a className="pagination-link" aria-label="Goto page 47">47</a></li>
+    {pagesNumber && pagesNumber.map((number)=>( 
+    <li><a key={number} className="pagination-link is-current" aria-label="Page 46" aria-current="page" onClick={()=>paginate(number)}>{number} </a></li>
+    )) }
     <li><span className="pagination-ellipsis">&hellip;</span></li>
     <li><a className="pagination-link" aria-label="Goto page 86">86</a></li>
   </ul>

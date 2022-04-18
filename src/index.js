2a2f5d3ba448da2d23dcab49cabd855309a6d670
zerontec <zerontecnologia1@gmail.com>
2022-04-18 11:37:31 -0430

    index and component dashboard creation

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Dashboard from './pages/Dashboard/Dashborad';
-import Landing from './pages/Landing/Landing';
-import Aside from './components/Aside/Aside';
-import NavbarP from './components/Navbar/Navbar';
-import InfoF from './components/InfoF/InfoF';
-import Footer from './components/Footer/Footer';
+import {Provider} from 'react-redux';
+import {store} from './Redux/stored/index'
+
+
 import 'bulma/css/bulma.min.css'
 
 ReactDOM.render(
--- src/pages/Dashboard/Dashborad.jsx
+++ src/pages/Dashboard/Dashborad.jsx
@@ -1,136 +1,148 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import NavbarP from '../../components/Navbar/Navbar';
 import Paginations from '../../components/Paginations/Pagination';
+import { getPokemon, getPokemonList } from '../../Redux/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import Card from '../../components/Card/Card';
+import Loading from '../../components/Loading/Loading';
+import _ from "lodash";
+import './ListStyles.css'
 
 
-const Dashboard = () => {
+
+
+const Dashboard = (props) => {
+ const [search, setSeacrh] = useState();
+ const dispatch = useDispatch();
+ const PokemonList = useSelector((state) => state.PokemonList);
+ 
+
+  //useEfect
+
+  useEffect(() => {
+    FetchData(1);
+  
+  }, []);
+
+  const FetchData = (page = 1) => {
+
+      dispatch(getPokemonList(page));
+
+
+  }
+
+  const ShowData = () => {
+    if(PokemonList.Loading){
+
+      <p>Loading...</p>
+    }
+
+if(!_.isEmpty(PokemonList.data)){
+
 
 return(
+  <div className="card">
+   
+{PokemonList.data.map((el) => {
+
+
+return(
+  <div className="card-image">
+    <figure className="Imagea">
+   <img alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${el.name}.jpg`}/>
+    </figure>
+
+
+  <div className="card-content">
+  <div className="media">
+      <div className="media-left">
+          <figure className="image is-48x48">
+              <img src={`https://img.pokemondb.net/artwork/large/${el.name}.jpg`}/>
+          </figure>
+      </div>
+      <div className="media-content">
+          <p className="title is-4">{el.name}</p>
+          <p className="subtitle is-6">@1johnsmith</p>
+      </div>
+  </div>
+
+  <div className="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      <a href="#">#css</a> <a href="#">#responsive</a>
+      <br />
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+  </div>
+</div>
+</div>
+)
+
+
+})}
+ 
+</div>
+  
+)
+
+}
+  }
+
+
+
+
+ 
+  return (
 
 
     <div>
 
-<section className="section">
-       <div className='container '>
-                
-        <div className="column ">
-                 
-                 <div className="columns  ">
-                     <div className="column ">
-                         <div className="card">
-                             <div className="card-image">
-                               <figure className="image is-4by3">
-                                 <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-                               </figure>
-                             </div>
-                             <div className="card-content">
-                               <div className="media">
-                                 <div className="media-left">
-                                   <figure className="image is-48x48">
-                                     <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
-                                   </figure>
-                                 </div>
-                                 <div className="media-content">
-                                   <p className="title is-4">John Smith</p>
-                                   <p className="subtitle is-6">@johnsmith</p>
-                                 </div>
-                               </div>
-                           
-                               <div className="content">
-                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                 Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                                 <a href="#">#css</a> <a href="#">#responsive</a>
-                                 <br/>
-                                 <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                               </div>
-                             </div>
-                           </div>
-<hr/>
-                       
-                           
-                     </div>
-                  
-                     <div className="column">
-                         <div className="card">
-                             <div className="card-image">
-                               <figure className="image is-4by3">
-                                 <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
-                               </figure>
-                             </div>
-                             <div className="card-content">
-                               <div className="media">
-                                 <div className="media-left">
-                                   <figure className="image is-48x48">
-                                     <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
-                                   </figure>
-                                 </div>
-                                 <div className="media-content">
-                                   <p className="title is-4">John Smith</p>
-                                   <p className="subtitle is-6">@johnsmith</p>
-                                 </div>
-                               </div>
-                           
-                               <div className="content">
-                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                 Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                                 <a href="#">#css</a> <a href="#">#responsive</a>
-                                 <br/>
-                                 <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                               </div>
-                             </div>
-                           </div>
-                           <hr/>
-                     </div>
-                  
-                   </div>
+      <section className="section">
+        <div className='container '>
 
-      
-  
+          <div className="column ">
 
-  {/*   <p className="bd-notification is-primary">
-        Second column with more content. 
-        This is so you can see the vertical 
-        alignment.
-        Second column with more content. 
-        This is so you can see the vertical 
-        alignment
-        Second column with more content. 
-        This is so you can see the vertical 
-        alignment
-        Second column with more content. 
-        This is so you can see the vertical 
-        alignment
-        Second column with more content. 
-        This is so you can see the vertical 
-        alignment
-        Second column with more content. 
-        This is so you can see the vertical 
-        alignment
-        Second column with more content. 
-        This is so you can see the vertical 
-        alignment</p>
-         */}
-      
+            <div className="columns  ">
+              <div className="column ">
 
-  
-<Paginations/>
+
+<ShowData/>
 
 
 
-</div>
+              </div>
 
- 
-  </div>
+              
+
+            </div>
 
 
-  </section>
-  </div>
 
-      
-        
-       
 
-)
+
+{/* 
+            <Paginations
+              pokemonPerpage={pokemonPerpage}
+              allPokemons={allPokemons}
+              paginate={paginate}
+
+            /> */}
+
+
+
+          </div>
+
+
+        </div>
+
+
+      </section>
+    </div>
+
+
+
+
+
+  )
 
 
 }
